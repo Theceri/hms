fe0e3d043ca6a117b074feb7b82b16081d7060f6
Paul Theceri <paul.theceri@gmail.com>
2021-07-30 03:12:45 +0300

    changes so I can push to Heroku

--- configs/base_config.py
+++ configs/base_config.py
@@ -3,7 +3,7 @@ import os
 
 class Base:
     FLASK_APP = os.environ.get("FLASK_APP")
-    SQLALCHEMY_TRACKMODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_hex(16)
 
 class Development(Base):
@@ -14,10 +14,10 @@ class Development(Base):
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 class Staging(Base):
-    DATABASE = "purchases"
-    POSTGRES_USER = "postgres"
-    POSTGRES_PASSWORD = "1234"
-    SQLALCHEMY_DATABASE_URI= "172.17.0.1"
+    DATABASE = "d5pjjee5639vdt"
+    POSTGRES_USER = "lwkpjzajnzxfbi"
+    POSTGRES_PASSWORD = "a00b3c6741edc36ddf796ea948f820a58a90cf07104be13f7e0781034cb2d572"
+    SQLALCHEMY_DATABASE_URI= "postgres://lwkpjzajnzxfbi:a00b3c6741edc36ddf796ea948f820a58a90cf07104be13f7e0781034cb2d572@ec2-54-155-35-88.eu-west-1.compute.amazonaws.com:5432/d5pjjee5639vdt"
 
 class Production(Base):
     pass
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-from flask_marshmallow import Marshmallow
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 import os
 
 app = Flask(__name__)
-
-ma = Marshmallow(app)
 
 from configs.base_config import *
 app.config.from_object(Development)
